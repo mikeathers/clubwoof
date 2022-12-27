41f9f2ca74b499846f8a291438bcc482a5aa8144
Mike Atherton <athers_05@hotmail.co.uk>
2022-12-27 14:03:04 +0000

    fix: adds correct out path 4

--- .github/deploy/src/infra/backend-stack.ts
+++ .github/deploy/src/infra/backend-stack.ts
@@ -7,7 +7,7 @@ export class BackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    console.log('hey 3')
+    console.log('hey 4')
     const {userPool} = new UserPoolConstruct(this)
     const {userPoolClient} = new UserPoolClientConstruct(this, userPool)
     const {identityPool} = new IdentityPoolConstruct(this, userPool, userPoolClient)
