5b96b895efc355849799e91200799515c9ec8d64
Mike Atherton <athers_05@hotmail.co.uk>
2022-12-30 09:08:01 +0000

    fix: adds bucket deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack5: false,
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
