40397d9aa35d5ecf0e4c36dc8aa3b4e83c32fec6
Mike Atherton <athers_05@hotmail.co.uk>
2022-12-30 17:10:17 +0000

    fix: fixes deployment 2

---
+++ .github/deploy/src/functions/mapping-function.ts
@@ -0,0 +1,1 @@
+//@ts-ignorefunction handler(event) {  var request = event.request;  var uri = request.uri;  if (uri.endsWith('/')) {    request.uri += 'index.html';  } else if (!uri.includes('.')) {    request.uri += '/index.html';  }  return request;}
