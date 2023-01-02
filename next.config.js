b45845b26a427b867ccc4d81ee925afe733ea978
Mike Atherton <athers_05@hotmail.co.uk>
2023-01-02 15:00:34 +0000

    feat: adds commit hooks 2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack5: false,
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
