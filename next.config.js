f1ee8870cb0f6dad6a8f5a91e14a16d8ff339d48
Mike Atherton <athers_05@hotmail.co.uk>
2023-01-06 15:57:18 +0000

    feat: run a build and deploy 3

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  distDir: '_next',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 }
+
+module.exports = nextConfig
