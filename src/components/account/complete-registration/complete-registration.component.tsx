e84e1f4b6f174ac5df8601e5dbd9203bb61c1ab8
Mike Atherton <athers_05@hotmail.co.uk>
2023-01-15 15:27:30 +0000

    fix: lint issues

--- src/components/account/complete-registration/complete-registration.component.tsx
+++ src/components/account/complete-registration/complete-registration.component.tsx
@@ -1,12 +1,14 @@
 import {useCompleteRegistrationHook} from './use-complete-registration.hook'
-import {Layout} from "../../shared";
+import {Layout} from '../../shared'
 
 export const CompleteRegistration: React.FC = () => {
   const {loginSuccessful} = useCompleteRegistrationHook()
 
   if (loginSuccessful) {
     return (
-      <Layout bubbleOnePositioning={} bubbleTwoPositioning={}
+      <Layout>
+        <p>You&apos;ve logged in</p>
+      </Layout>
     )
   }
   return <div>complete reg</div>
