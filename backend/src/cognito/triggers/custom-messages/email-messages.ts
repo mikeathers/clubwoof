7496ed1e767798b240f0ddcf3e59186ae63ed9d9
Mike Atherton <athers_05@hotmail.co.uk>
2023-01-26 13:14:54 +0000

    feat: adds styling to register email

--- backend/src/cognito/triggers/custom-messages/email-messages.ts
+++ backend/src/cognito/triggers/custom-messages/email-messages.ts
@@ -1,19 +1,26 @@
 import CustomMessage from './custom-message'
 import _ from 'lodash'
+import {colors} from '@clubwoof-styles'
 
 export const completeSignup = (props: CustomMessage, firstName: string) => {
   return {
-    emailSubject: `Confirm your clubwoof account!`,
-    emailMessage: `<div style={{height: '100vh'}}>
-      <div
-        style='display: flex, justifyContent: center, flex-direction: column,
-          padding: 16px'       
-      >
+    emailSubject: `Confirm your clubwoof account! 🐶`,
+    emailMessage: `
+    <div>
+      <div>
         <h1>Hi ${_.startCase(_.camelCase(firstName))} 👋</h1>
         <p>Thanks for signing up!</p>
         <p>Click on the link to activate your account and get started.</p>
-        <a href="${props.FRONTEND_LINKS.SEND_CODE_POST_SIGN_UP}">Activate my account!</a>
+        <br />
+        <br />
+        <a style="border-radius: 6px; text-decoration: none; font-weight: 500; padding: 12px; background-color:${
+          colors.lightBlue
+        }; color: ${colors.pureWhite} " href="${
+      props.FRONTEND_LINKS.SEND_CODE_POST_SIGN_UP
+    }">Activate my account!</a>
       </div>
+      <br />
+      <br />
       <br />
       <br />
        <img          
